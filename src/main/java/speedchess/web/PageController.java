752c8e995e8241573563af86ca8ebe04dfa06546
mats <myts@fastmail.com>
2022-01-06 19:16:14 +0100

    Image page working! Thanks, Thymeleaf

--- src/main/java/speedchess/web/PageController.java
+++ src/main/java/speedchess/web/PageController.java
@@ -1,12 +1,23 @@
 package speedchess.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PageController {
+    private final Logger logger = LoggerFactory.getLogger(PageController.class);
+
     @GetMapping("/")
     public String index() {
+        logger.info("Serving index page ...");
         return "index";
+    }
+
+    @GetMapping("/image")
+    public String getImage() {
+        logger.info("Serving image page ...");
+        return "image";
     }
 }
